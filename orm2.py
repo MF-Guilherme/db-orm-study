57662f338fdc899220f588243d9c9968b4d4ebb8
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-02-14 10:22:30 -0300

    Select com SqlAlchemy

--- orm2.py
+++ orm2.py
@@ -19,9 +19,17 @@ def retorna_session():
 
 session = retorna_session()
 
-x = Pessoa(nome='Joao', usuario='joao', senha='1234')
-y = Pessoa(nome='', usuario='joao', senha='1234')
+x = session.query(Pessoa).all() # pegando tudo que está no banco de dados (retorna uma lista de objetos)
+# for i in x:
+#     print(f'ID: {i.id}')
+#     print(f'NOME: {i.nome}')
+#     print(f'SENHA: {i.senha}')
+#     print('-' * 20)
 
-session.add_all([x, y])
-session.rollback()
-session.commit()
+# x = session.query(Pessoa).filter(Pessoa.nome == "Guilherme").all() # filtrando por uma única condição
+
+# x = session.query(Pessoa).filter(Pessoa.nome == 'Guilherme').filter(Pessoa.usuario == 'guimont').all() # filtrando com mais de uma condição (and)
+x = session.query(Pessoa).filter_by(nome='Guilherme', usuario='guimont') # forma simplificada, utilizando o nome da coluna da tabela
+
+for i in x:
+    print(i.id)
